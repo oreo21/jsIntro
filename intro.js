db2c840158611eb39668ed5240d782e5d7650cff
oreo21 <reok99@gmail.com>
2016-12-16 12:37:57 -0500

    added some stuff, doesn't work

--- intro.js
+++ intro.js
@@ -1,12 +1,28 @@
 var b = document.getElementById("b");
 var ol = document.getElementById("thelist");
-var f = function(e){
+var f1 = function(e){
     console.log(e);
     var node = document.createElement("li");
     var text = document.createTextNode("Yo yo yo!");
     node.appendChild(text);
     ol.appendChild(node);
 }
-b.addEventListener('click', f);
+b.addEventListener('click', f1);
+
+var h = document.getElementById("h");
+var hOrig = h.innerHTML;
+var f2 = function(e){
+    console.log(e);
+    newtext = this;
+    h.innerHTML = newtext;
+}
+var remove = function(n){
+    var li = document.getElementById("li");
+    li[n].remove();
+}
     
+    
+li.addEventListener('mouseover', f2);
+li.addEventListener('mouseout', f3);
+li[7].addEventListener('click' remove);
     
